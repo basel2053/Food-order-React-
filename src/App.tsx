d99cdc39f5951b23cb6d8a777b97b0a20149bd03
unknown <baselsalah2053@gmail.com>
2023-01-07 16:07:48 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+	return (
+		<div className='App'>
+			<h1 className='text-7xl font-bold underline text-blue-600'>Hello world</h1>
+		</div>
+	);
+}
+
+export default App;
