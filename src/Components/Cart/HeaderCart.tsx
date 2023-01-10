a84e6218a72ad2a588138feecfd12d217a5213e1
unknown <baselsalah2053@gmail.com>
2023-01-11 00:47:56 +0200

    adding a little animation on adding item to cart

--- src/Components/Cart/HeaderCart.tsx
+++ src/Components/Cart/HeaderCart.tsx
@@ -1,9 +1,32 @@
+import { useEffect, useState, useContext } from 'react';
 import CartButtonContent from './CartButtonContent';
+import MealsContext from '../../context/MealsContext';
 
 const HeaderCart = (props: { onOrder: React.MouseEventHandler }) => {
+	const mealsCtx = useContext(MealsContext);
+	const [btnisHighlighted, setBtnIsHighlighted] = useState(false);
+	const { items } = mealsCtx;
+
+	useEffect(() => {
+		if (items.length === 0) {
+			return;
+		}
+		setBtnIsHighlighted(true);
+		const timer = setTimeout(() => {
+			setBtnIsHighlighted(false);
+		}, 1500);
+
+		return () => {
+			clearTimeout(timer);
+		};
+	}, [items]);
+	const btnClasses = btnisHighlighted ? `animate-pulse` : '';
 	return (
 		<div
-			className='bg-secondary rounded-3xl w-1/6 py-3 cursor-pointer hover:bg-[#2c0d00] active:bg-[#2c0d00] transition-all '
+			className={
+				`bg-secondary rounded-3xl w-1/6 py-3 cursor-pointer hover:bg-[#2c0d00] active:bg-[#2c0d00] transition-all ` +
+				btnClasses
+			}
 			onClick={props.onOrder}
 		>
 			<CartButtonContent />
