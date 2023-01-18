4e1587b3ec19cba8d6808060afc32c0ee7496e83
unknown <baselsalah2053@gmail.com>
2023-01-18 15:07:45 +0200

    working with json-server instead of dummy info

--- src/Components/Meals/Meals.tsx
+++ src/Components/Meals/Meals.tsx
@@ -1,35 +1,52 @@
 import MealItem from './MealItem';
 import Card from '../UI/Card';
-const dummyMeals = [
-	{
-		id: 'm1',
-		name: 'Sushi',
-		description: 'Finest fish and veggies',
-		price: 22.99,
-	},
-	{
-		id: 'm2',
-		name: 'Schnitzel',
-		description: 'A german specialty!',
-		price: 16.5,
-	},
-	{
-		id: 'm3',
-		name: 'Barbecue Burger',
-		description: 'American, raw, meaty',
-		price: 12.99,
-	},
-	{
-		id: 'm4',
-		name: 'Green Bowl',
-		description: 'Healthy...and green...',
-		price: 18.99,
-	},
-];
+import { useEffect, useState } from 'react';
+interface IMeal {
+	id?: string;
+	name: string;
+	description: string;
+	price: number;
+}
 const Meals = () => {
+	const [meals, setMeals] = useState([] as IMeal[]);
+	const [isLoading, setIsLoading] = useState(true);
+	const [httpError, setHttpError] = useState<unknown>();
+
+	const getMeals = async () => {
+		const res = await fetch('http://localhost:3000/meals');
+		if (!res.ok) {
+			throw new Error('Something went wrong!');
+		}
+		const data = await res.json();
+		setMeals(data);
+		setIsLoading(false);
+	};
+
+	useEffect(() => {
+		getMeals().catch(err => {
+			setIsLoading(false);
+			setHttpError((err as Error).message);
+		});
+	}, []);
+
+	if (isLoading) {
+		return (
+			<Card className='bg-white w-7/12 m-auto my-9 px-8 pb-8'>
+				<p>Loading...</p>
+			</Card>
+		);
+	}
+	if (httpError) {
+		return (
+			<Card className='bg-white w-7/12 m-auto my-9 px-8 pb-8'>
+				<p>{String(httpError)}</p>
+			</Card>
+		);
+	}
+
 	return (
 		<Card className='bg-white w-7/12 m-auto my-9 px-8 pb-8'>
-			{dummyMeals.map(meal => (
+			{meals.map(meal => (
 				<MealItem key={meal.id} id={meal.id} name={meal.name} description={meal.description} price={meal.price} />
 			))}
 		</Card>
